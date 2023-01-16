c683aab6ba08060ef69ac2b11ac3f99744d7a420
fawda123 <mbafs2012@gmail.com>
2023-01-16 12:35:53 -0500

    initial map and matrix

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,8 +1,21 @@
 library(tbeptools)
 library(here)
+library(sf)
 
 # tbbi scores ---------------------------------------------------------------------------------
 
-tbbiscr <- anlz_tbbiscr(benthicdata)
+tbbiscr <- anlz_tbbiscr(benthicdata) %>%
+  select(-TotalAbundance, -SpeciesRichness, -Salinity)
 
 save(tbbiscr, file = here('data/tbbiscr.RData'))
+
+# segment polygons ----------------------------------------------------------------------------
+
+load(file = here('data/segmask.RData'))
+
+segs <- tbsegshed %>%
+  st_make_valid() %>%
+  st_intersection(segmask) %>%
+  st_simplify(dTolerance = 10, preserveTopology = TRUE)
+
+save(segs, file = here('data/segs.RData'))
