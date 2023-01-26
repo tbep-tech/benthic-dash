5066875b0a0bc61d39fec7c79166809144f920bd
fawda123 <mbafs2012@gmail.com>
2023-01-26 10:57:52 -0500

    saving all preprocessed data for app and leafletproxy for benthic map

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1,6 +1,11 @@
 library(tbeptools)
 library(here)
+library(dplyr)
+library(filter)
 library(sf)
+library(leaflet)
+
+cols <- c('#CC3231', '#E9C318', '#2DC938')
 
 # tbbi scores ---------------------------------------------------------------------------------
 
@@ -9,7 +14,7 @@ tbbiscr <- anlz_tbbiscr(benthicdata) %>%
 
 save(tbbiscr, file = here('data/tbbiscr.RData'))
 
-# segment polygons ----------------------------------------------------------------------------
+# tb segments -------------------------------------------------------------
 
 load(file = here('data/segmask.RData'))
 
@@ -19,3 +24,58 @@ segs <- tbsegshed %>%
   st_simplify(dTolerance = 10, preserveTopology = TRUE)
 
 save(segs, file = here('data/segs.RData'))
+
+# median tbbi scores by segment -------------------------------------------
+
+load(file = here('data/tbbiscr.RData'))
+
+benmed <- tbbiscr %>%
+  tbeptools::anlz_tbbimed(bay_segment = c("HB", "OTB", "MTB", "LTB", "TCB", "MR", "BCB")) %>%
+  mutate(
+    outcome = case_when(
+      TBBICat == 'Good' ~ cols[3],
+      TBBICat == 'Fair' ~ cols[2],
+      TBBICat == 'Poor' ~ cols[1]
+    )
+  )
+
+save(benmed, file = here('data/benmed.RData'))
+
+# benthic points all years as sf ------------------------------------------
+
+# points
+benpts <- tbbiscr %>%
+  filter(ProgramName %in% c('Benthic Monitoring')) %>%
+  filter(TBBICat != 'Empty Sample') %>%
+  filter(AreaAbbr %in% c("HB", "OTB", "MTB", "LTB", "TCB", "MR", "BCB")) %>%
+  mutate(
+    outcome = case_when(
+      TBBICat == 'Healthy' ~ cols[3],
+      TBBICat == 'Intermediate' ~ cols[2],
+      TBBICat == 'Degraded' ~ cols[1]
+    )
+  ) %>%
+  sf::st_as_sf(coords = c('Longitude', 'Latitude'), crs = 4326)
+
+save(benpts, file = here('data/benpts.RData'))
+
+# parameter lookup --------------------------------------------------------
+
+# parameter lookup
+prmlkup <- sedimentdata %>%
+  select(SedResultsType, Parameter) %>%
+  unique() %>%
+  filter(SedResultsType %in% c('Metals', 'Organics') | grepl('TC|TIC|TOC', Parameter)) %>%
+  arrange(SedResultsType, Parameter)
+
+save(prmlkup, file = here('data/prmlkup.RData'))
+
+# benthic matrix ----------------------------------------------------------
+
+fml <- 'Lato'
+
+load(file = here('data/tbbiscr.RData'))
+
+tbbimat <- tbeptools::show_tbbimatrix(tbbiscr, family = fml, txtsz = NULL)
+
+save(tbbimat, file = here('data/tbbimat.RData'))
